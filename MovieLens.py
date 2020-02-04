7cefdb86569bf6797ee9dc37c1e1a232006d0aac
Hamza JGUERIM <hamzajg@gmail.com>
2020-02-04 20:24:53 +0100

    Working on MovieLens.py..

--- MovieLens.py
+++ MovieLens.py
@@ -1,8 +1,19 @@
-class MovieLens:
+import os
 
-  def loadMovieLensLatestSmall(self):
+from surprise import Reader
+
+class MovieLens:
     movieID_to_name = {}
     name_to_movieID = {}
     ratingPath = './ml-latest-small/rating.csv'
     moviesPath = './ml-latest-small/movies.csv'
-    print('loadMovieLensLatestSmall')
+
+    def loadMovieLensLatestSmall(self):
+        os.chdir(os.path.dirname(sys.argv[0]))
+
+        ratingDataset = 0
+        self.movieID_to_name = {}
+        self.name_to_movieID = {}
+
+        reader = Reader(line_format='user item reating timestamp', sep=',', skipline=1) 
+        print('loadMovieLensLatestSmall')
