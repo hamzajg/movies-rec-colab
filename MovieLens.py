d08e0f558dd0a341993b35850d1199f7ce1421df
Hamza JGUERIM <hamzajg@gmail.com>
2020-02-04 20:15:05 +0100

    Working on MovieLens.py..

--- MovieLens.py
+++ MovieLens.py
@@ -1,4 +1,8 @@
 class MovieLens:
 
   def loadMovieLensLatestSmall(self):
+    movieID_to_name = {}
+    name_to_movieID = {}
+    ratingPath = './ml-latest-small/rating.csv'
+    moviesPath = './ml-latest-small/movies.csv'
     print('loadMovieLensLatestSmall')
